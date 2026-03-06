e33637e2ce8d330cb4807d5d4d499be7e51821d0
ibrahimkhrekani7-eng <ibrahimkhrekani7@gmail.com>
2026-03-06 07:58:03 +0300

    Add files via upload

---
+++ public/sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'kurdflux-v1';
+const urlsToCache = [
+  './',
+  'https://cdn.plyr.io/3.7.8/plyr.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+  'https://cdn.jsdelivr.net/npm/hls.js@latest',
+  'https://cdn.plyr.io/3.7.8/plyr.polyfilled.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache opened');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
